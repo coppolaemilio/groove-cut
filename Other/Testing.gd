e059b38c7a9d0bf0ff5c25e63082f5e13f463160
Emilio Coppola <coppolaemilio@gmail.com>
2020-01-20 14:09:20 +0100

    discovery and extracting songs in user dir

---
+++ Other/Testing.gd
@@ -0,0 +1,28 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	discover_songs()
+	#get_tree().quit()
+	#open_working_directory()
+
+func discover_songs():
+	var directory = Directory.new()
+	for file in global.list_dir('user://'):
+		if '.zip' in file:
+			print(file)
+			var current_file = OS.get_user_data_dir() + '/' + file
+			var target_dir = OS.get_user_data_dir() + '/' + file.replace('.zip', '')
+			if directory.dir_exists(target_dir) == false:
+				match OS.get_name():
+					'OSX':
+						var out = []
+						OS.execute('unzip', [current_file, '-d', target_dir], true, out, true)
+						print(out)
+
+func open_working_directory():
+	match OS.get_name():
+		'OSX':
+			OS.execute('open', [OS.get_user_data_dir()], false)
+		'_':
+			print('[!] Can\'t detect OS')
--- VRMain.gd
+++ VRMain.gd
@@ -1,14 +1,16 @@
 extends Spatial
 
-# Code from https://docs.godotengine.org/en/3.1/tutorials/vr/vr_starter_tutorial.html
+var vr_mode = false
+
 func _ready():
-	var VR = ARVRServer.find_interface("OpenVR")
-	if VR and VR.initialize():
-		get_viewport().arvr = true
-		get_viewport().hdr = false
-		
-		OS.vsync_enabled = false
-		#Engine.target_fps = 90
-		# Also, the physics FPS in the project settings is also 90 FPS. This makes the physics
-		# run at the same frame rate as the display, which makes things look smoother in VR!
-# -------------------------------------
+	if vr_mode:
+		var VR = ARVRServer.find_interface("OpenVR")
+		if VR and VR.initialize():
+			get_viewport().arvr = true
+			get_viewport().hdr = false
+			
+			OS.vsync_enabled = false
+			#Engine.target_fps = 90
+	else:
+		$OVRFirstPerson.queue_free()
+		$Camera.current = true
