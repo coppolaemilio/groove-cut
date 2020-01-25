a1ae0d9b06472bb584e1acb596f502dcf7bd4fc3
Xantomen <xantomen@gmail.com>
2020-01-25 15:22:25 +0100

    Changing Note Direction array for enum

--- SongPlayer/SongPlayer.gd
+++ SongPlayer/SongPlayer.gd
@@ -1,27 +1,50 @@
 extends Node
 
+enum DIRECTION {
+	Down,
+	Up,
+	Left,
+	Right,
+	DownLeft,
+	DownRight,
+	UpLeft,
+	UpRight,
+	All
+}
+
+var directionEnumSize = 9;
+
 var Note = load("res://GameProps/Note.tscn")
 
 var current_song = []
 
 func _on_Timer_timeout():
 	var new_note = Note.instance()
-	var directions = ['up', 'down', 'left', 'right', 'all']
-	var current_direction = directions[randi()%directions.size()]
+	var current_direction = randi()%directionEnumSize
+	
+	current_direction = DIRECTION.UpRight;
 	
 	match current_direction:
-		'up':
+		DIRECTION.Down:
+			new_note.rotate_z(deg2rad(0.0))
+		DIRECTION.Up:
 			new_note.rotate_z(deg2rad(180.0))
-		'left':
-			new_note.rotate_z(deg2rad(270.0))
-		'right':
+		DIRECTION.Left:
+			new_note.rotate_z(deg2rad(-90.0))
+		DIRECTION.Right:
 			new_note.rotate_z(deg2rad(90.0))
-		'all':
+		DIRECTION.DownLeft:
+			new_note.rotate_z(deg2rad(-45.0))
+		DIRECTION.DownRight:
+			new_note.rotate_z(deg2rad(45.0))
+		DIRECTION.UpLeft:
+			new_note.rotate_z(deg2rad(-135.0))
+		DIRECTION.UpRight:
+			new_note.rotate_z(deg2rad(135.0))
+		DIRECTION.All:
 			new_note.get_node('DirectionalNote').visible = false
-	
-	if current_direction != 'all':
+		
+	if current_direction != DIRECTION.All:
 		new_note.get_node('AllDirectionsNote').visible = false
-	
-	print(current_direction)
 	
 	get_parent().get_node('Notes').add_child(new_note)
